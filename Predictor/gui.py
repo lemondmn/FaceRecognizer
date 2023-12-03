2371d37db23f9e3a68f0e910272c4073f74e1ed5
Humberto Maldonado <lemon_tea@outlook.es>
2023-12-03 15:56:24 -0600

    New detection

--- Predictor/gui.py
+++ Predictor/gui.py
@@ -4,8 +4,6 @@ from recognizer import Recognizer
 
 class RecognizerSetup:
     def __init__(self) -> None:
-        self.file = None
-        self.ip = None
         self.layout = [
             [sg.Text('Configuración')],
             [sg.Text('Archivo Eigenface\t'), sg.InputText(key='eigen', expand_x=True), sg.FileBrowse()],
@@ -20,28 +18,28 @@ class RecognizerSetup:
             if event == sg.WIN_CLOSED or event == 'Cerrar':
                 break
             elif event == 'Iniciar':
-                self.file = values['eigen']
-                self.ip = values['ip']
-                if not os.path.exists(self.file):
+                filepath = values['eigen']
+                ip = values['ip']
+                print(f"Archivo Eigenface: {filepath}, IP: {ip}")
+                if not os.path.exists(filepath):
                     sg.Popup('Error', 'El directorio no existe')                    
                 else:
                     break
         window.close()
-        return self.file, self.ip
+        return filepath, ip
 
 class MainUI:
     def __init__(self, path, ip) -> None:
-        self.path = path
-        self.ip = ip
-        self.recognizer = Recognizer(self.path, str(self.ip))
+        if ip == '': ip = None
+        self.recognizer = Recognizer(path, ip)
         
-        # if not ip == None:
-        #     if not self.recognizer.testConnection():
-        #         sg.Popup('Error', 'No se pudo establecer conexión con la cámara IP')
-        #         self.recognizer.destroy()
-        #         self.recognizer = None
-        #     else:
-        #         sg.Popup('Éxito', 'Se estableció conexión con la cámara IP')
+        if not ip == None:
+            if not self.recognizer.testConnection():
+                sg.Popup('Error', 'No se pudo establecer conexión con la cámara IP')
+                self.recognizer.destroy()
+                self.recognizer = None
+            else:
+                sg.Popup('Éxito', 'Se estableció conexión con la cámara IP')
 
         self.layout = [
             [sg.Image(filename='', key='image')],
